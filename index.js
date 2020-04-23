92b1e2c7d5c0310b6edae3cefb25fda6cb2a10fc
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2020-04-23 19:56:02 -0400

    added process.env.PORT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get("/ping", (req, res) => {
   res.status(200).json({ ping: "🏓" });
 });
 
-let port = 6001;
+let port = process.env.PORT || 6001;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
